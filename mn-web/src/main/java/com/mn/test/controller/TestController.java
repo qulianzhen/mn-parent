5ba9add281d6720b9f53e8f6b08336b4b660294a
zldtk <1094590717@qq.com>
2019-02-17 18:45:53 +0800

    update .gitignore

---
+++ mn-web/src/main/java/com/mn/test/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.mn.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: (测试Controller)
+ * @Author:Mloong
+ * @Date :create in 2019/2/14-22:18
+ * @Version 1.0
+ * @Modified By: qlz
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "This is a Test Controller!";
+    }
+}
