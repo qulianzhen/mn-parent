06e0e1354706950ceed20bbd18e1b58e21fb62b2
dn <1094590717@qq.com>
2019-02-19 17:04:19 +0800

    log4j2

--- mn-web/src/main/java/com/mn/dict/controller/SysDictController.java
+++ mn-web/src/main/java/com/mn/dict/controller/SysDictController.java
@@ -2,6 +2,8 @@ package com.mn.dict.controller;
 
 import com.mn.dict.entity.po.SysDict;
 import com.mn.dict.service.SysDictService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,13 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/sysDict")
 public class SysDictController {
+    private Logger logger = LoggerFactory.getLogger(SysDictController.class);
     @Autowired
     private SysDictService sysDictService;
 
     @GetMapping(value = "/list")
     public List<SysDict> list(){
+        logger.info("这是一个日志！");
         return sysDictService.list();
     }
 
